dddb5368406559a682474e61965c7936dc519805
AbdulHameedi <sanilinity507@gmail.com>
2023-07-30 23:40:13 +0100

    'nav-barcontd'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,12 @@
 <script>
 import { defineComponent } from 'vue';
 import NavBar from './NavBar.vue'
+import StickyMenu from './StickyMenu.vue'
 
 export default defineComponent({
     components:{
-        NavBar
+        NavBar,
+        StickyMenu
     },
     mounted(){
         let user = localStorage.getItem("user-info")
@@ -15,5 +17,6 @@ export default defineComponent({
 })
 </script>
 <template>
+    <StickyMenu />
     <NavBar />
 </template>
